47a6834fb305eca41d5c9d71d0b0d30059e8f602
Indra Rudianto <indrarudianto.official@gmail.com>
2020-05-26 22:32:46 +0700

    Updae enum values

--- tlr/enums.py
+++ tlr/enums.py
@@ -3,7 +3,7 @@ from enum import IntEnum, unique
 
 @unique
 class MeasurementType(IntEnum):
-    TEMPERATURE0 = 1  # vgms_l530_0
-    EMISSION = 2      # vgms_l530_1
-    TEMPERATURE2 = 3  # vgms_l530_2
-    TEMPERATURE3 = 4  # vgms_l530_3
+    TEMPERATURE0 = 0  # vgms_l530_0
+    EMISSION = 1      # vgms_l530_1
+    TEMPERATURE2 = 2  # vgms_l530_2
+    TEMPERATURE3 = 3  # vgms_l530_3
